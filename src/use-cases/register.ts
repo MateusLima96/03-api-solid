77be60335430e170b2b9a2fdcbee0463249b51dd
MateusLima96 <mateuslima020@outlook.com>
2023-11-14 16:48:30 -0300

    initial commit

---
+++ src/use-cases/register.ts
@@ -0,0 +1,39 @@
+import { UsersRepository } from "@/repositories/users-repository"
+import { hash } from "bcryptjs"
+
+interface RegisterUseCaseRequest {
+    name: string
+    email: string
+    password: string
+}
+
+//SOLID
+
+// D - Dependecy Inversion Principle (ao inves de instanciar as dependencias eu vou recebe-las como parametro)
+
+export class RegisterUseCase{
+  constructor (private usersRepository: UsersRepository){}
+  async execute({
+     name,
+     email,
+     password
+ }: RegisterUseCaseRequest){
+     
+     const password_hash = await hash(password, 6)
+ 
+     const userWithSameEmail = await this.usersRepository.findByEmail(email)
+ 
+     if(userWithSameEmail){
+       throw new Error('E-mail already exists.')
+     }
+ 
+     //const prismaUsersRepository = new PrismaUsersRepository
+ 
+     await this.usersRepository.create({
+       name,
+       email,
+       password_hash
+     })
+  
+    }
+}
