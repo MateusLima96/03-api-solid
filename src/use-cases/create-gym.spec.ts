2370b0fefe1f207139dc2979d8d678257e19d85a
MateusLima96 <mateuslima020@outlook.com>
2023-11-25 19:15:44 -0300

    add create gym use case

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,31 @@
+import { compare } from 'bcryptjs'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { RegisterUseCase } from './register'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+describe('Create Gym Use Case', () => {
+
+  let gymsRepository: InMemoryGymsRepository
+  let sut: CreateGymUseCase
+
+  beforeEach(()=>{
+    gymsRepository = new InMemoryGymsRepository
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    
+    const { gym } = await sut.execute({
+      title: 'John Doe',
+      description: '',
+      phone: '23',
+      latitude: 0,
+      longitude: 0
+    })
+
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
