6dbb1ef1e6569e2cb17592eae1b71329b3eacaff
MateusLima96 <mateuslima020@outlook.com>
2023-12-05 23:46:54 -0300

    add get user metrics

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -6,5 +6,4 @@ import {authenticate} from './controllers/authenticate'
 export async function appRoutes(app: FastifyInstance){
     app.post('/users', register)
     app.post('/sessions', authenticate)
-
 }
