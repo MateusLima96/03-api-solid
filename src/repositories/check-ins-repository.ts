e84a2e4df1dcce025ffa73e05cdf7a6af50a21ca
Mateus Lima <mateuslima020@outlook.com>
2024-03-03 12:39:24 -0300

    add check ins repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,6 +1,6 @@
 import { CheckIn, Prisma } from "@prisma/client";
 
-export interface CheckInsRepository{
+export interface CheckInsRepository {
 
     findById(id: string): Promise<CheckIn | null>
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
